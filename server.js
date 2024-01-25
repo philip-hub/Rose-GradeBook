8cc7ea4f2a3b13b3b0ddc1bebd2a7d18491870ab
philip-hub <56984782+philip-hub@users.noreply.github.com>
2024-01-25 23:38:34 +0000

    Added all the classes and added user verification to make sure only signed in users are using the site

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 3000
 const path = require('path');
 const bodyParser = require('body-parser');
 const http = require('http');
+const session = require('express-session');
 //const formidable = require('formidable');
 app.use(express.json()); // For JSON payloads
 app.use(express.urlencoded({ extended: false }));
@@ -14,15 +15,22 @@ app.use('/style', express.static('style'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'templates'));
 
+const jsonData = require('./2024_courseinfo_courseset.json'); // Adjust the path as necessary
+console.log(jsonData);
+
+app.use(session({
+  secret: 'your_secret_key', // A secret key for signing the session ID cookie
+  resave: false,              // Forces the session to be saved back to the session store
+  saveUninitialized: true,    // Forces a session that is "uninitialized" to be saved to the store
+  // other configuration options...
+}));
 
 
 function storeInfo(req, res, next) {
-    // Extract the number from the request body and store it in the request object
     req.number = String(req.body.number);
     req.major = String(req.body.major);
     console.log(req.number);
-
-    next(); // Proceed to the next middleware or route handler
+    next(); 
 }
 
 
@@ -40,7 +48,8 @@ app.post('/signup', function (req,res){
 
 
 app.use('/verify', storeInfo, function (req, res, next) {
-  // Render the welcome page with the provided number
+  req.session.number = req.number;
+  req.session.major = req.major;
   res.render('welcome', { name: req.number, major:req.major });
 
   // Log that the verify page was accessed
@@ -48,11 +57,12 @@ app.use('/verify', storeInfo, function (req, res, next) {
 });
 
 // '/verifynumber' route
-app.use('/verifycode', function (req, res, next) {
+app.use('/verifycode',storeInfo, function (req, res, next) {
   // Access the stored number from the request object
-    let vCode = req.body.vcode;
-    let number = req.number;
-    console.log(number);
+    const vCode = req.body.vcode;
+    const number = req.session.number;
+    const major = req.session.major;
+    console.log(String(number));
     // Extract the number from the request body
     gpaList=["3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25", "3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25","3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25", "3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25","2.75", "2.5", "2.25", "3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25"]
     freshmanGPA = 3.75
@@ -75,10 +85,11 @@ app.use('/verifycode', function (req, res, next) {
 });
 
 
-app.use('/home', function (req, res, next) {
+app.use('/home',storeInfo, function (req, res, next) {
   // Access the stored number from the request object
     let vCode = req.body.vcode;
-    let number = req.number;
+    const number = req.session.number;
+    const major = req.session.major;
     console.log(number);
     // Extract the number from the request body
     gpaList=["3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25", "3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25","3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25", "3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25","2.75", "2.5", "2.25", "3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25"]
@@ -98,11 +109,14 @@ app.use('/home', function (req, res, next) {
 
 
 
-app.use('/classsearch', function (req, res, next) {
+app.use('/classsearch',storeInfo, function (req, res, next) {
   classList =["ES201","MA222"];
+  const number = req.session.number;
+  const major = req.session.major;
   search = String(req.body.search).toUpperCase();
-  if (classList.includes(search)){
-    res.render('classpage',{className:search});
+  const courseExists = search in jsonData;
+  if (courseExists){
+    res.render('classpage',{className:search,number:number});
 }
   else{
     res.render('classnotfound');
@@ -111,87 +125,107 @@ app.use('/classsearch', function (req, res, next) {
 
 gpaList=["3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25", "3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25","3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25", "3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25","2.75", "2.5", "2.25", "3.56", "3.5", "3.4", "3.3", "3.25", "3.0", "2.75", "2.5", "2.25"]
 app.use('/Biochemistry', function (req, res, next) {
-  
-  res.render('major',{major:"Biochemistry",gpa:gpaList[0]});
+  const BIOClasses = Object.keys(jsonData).filter(key => jsonData[key] === "BIO");
+  res.render('major',{major:"Biochemistry",gpa:gpaList[0],classes:BIOClasses});
   
 });
 
 
 
 app.use('/Biochemistry-and-Molecular-Biology', function (req, res, next) {
-  res.render('major', { major: "Biochemistry and Molecular Biology", gpa: gpaList[1] });
+  const BIOClasses = Object.keys(jsonData).filter(key => jsonData[key] === "BIO");
+  res.render('major', { major: "Biochemistry and Molecular Biology", gpa: gpaList[1],classes:BIOClasses });
 });
 
 app.use('/Biology', function (req, res, next) {
-  res.render('major', { major: "Biology", gpa: gpaList[2] });
+  const BIOClasses = Object.keys(jsonData).filter(key => jsonData[key] === "BIO");
+  res.render('major', { major: "Biology", gpa: gpaList[2],classes:BIOClasses });;
 });
 
 app.use('/Biomathematics', function (req, res, next) {
-  res.render('major', { major: "Biomathematics", gpa: gpaList[3] });
+  const BMTHClasses = Object.keys(jsonData).filter(key => jsonData[key] === "BMTH");
+  res.render('major', { major: "Biomathematics", gpa: gpaList[3],classes:BMTHClasses });
 });
 
 app.use('/Biomedical-Engineering', function (req, res, next) {
-  res.render('major', { major: "Biomedical Engineering", gpa: gpaList[4] });
+
+  const BEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "BE");
+  res.render('major', { major: "Biomedical Engineering", gpa: gpaList[4],classes:BEClasses });;
 });
 
 app.use('/Chemical-Engineering', function (req, res, next) {
-  res.render('major', { major: "Chemical Engineering", gpa: gpaList[5] });
+  const CHEMEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "CHE");
+  res.render('major', { major: "Chemical Engineering", gpa: gpaList[5],classes:CHEMEClasses });
 });
 
 app.use('/Chemistry', function (req, res, next) {
-  res.render('major', { major: "Chemistry", gpa: gpaList[6] });
+  const CHEMClasses = Object.keys(jsonData).filter(key => jsonData[key] === "CHEM");
+  res.render('major', { major: "Chemistry", gpa: gpaList[6],classes:CHEMClasses });;
 });
 
 app.use('/Civil-Engineering', function (req, res, next) {
-  res.render('major', { major: "Civil Engineering", gpa: gpaList[7] });
+  const CEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "CE");
+  res.render('major', { major: "Civil Engineering", gpa: gpaList[7],classes:CEClasses });
 });
 
 app.use('/Computational-Science', function (req, res, next) {
-  res.render('major', { major: "Computational Science", gpa: gpaList[8] });
+  const MAClasses = Object.keys(jsonData).filter(key => jsonData[key] === "MA");
+  res.render('major', { major: "Computational Science", gpa: gpaList[8],classes:MAClasses });
 });
 
 app.use('/Computer-Engineering', function (req, res, next) {
-  res.render('major', { major: "Computer Engineering", gpa: gpaList[9] });
+  const ECEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "ECE");
+  res.render('major', { major: "Computer Engineering", gpa: gpaList[9],classes:ECEClasses });
 });
 
 app.use('/Computer-Science', function (req, res, next) {
-  res.render('major', { major: "Computer Science", gpa: gpaList[10] });
+  const CSSEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "CSSE");
+  res.render('major', { major: "Computer Science", gpa: gpaList[10],classes:CSSEClasses });
 });
 
 app.use('/Data-Science', function (req, res, next) {
-  res.render('major', { major: "Data Science", gpa: gpaList[11] });
+  const CSSEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "CSSE");
+  res.render('major', { major: "Data Science", gpa: gpaList[11],classes:CSSEClasses });
 });
 
 app.use('/Electrical-Engineering', function (req, res, next) {
-  res.render('major', { major: "Electrical Engineering", gpa: gpaList[12] });
+  const ECEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "ECE");
+  res.render('major', { major: "Electrical Engineering", gpa: gpaList[12],classes:ECEClasses });
 });
 
 app.use('/Engineering-Design', function (req, res, next) {
-  res.render('major', { major: "Engineering Design", gpa: gpaList[13] });
+  const ENGDClasses = Object.keys(jsonData).filter(key => jsonData[key] === "ENGD");
+  res.render('major', { major: "Engineering Design", gpa: gpaList[13],classes:ENGDClasses});
 });
 
 app.use('/International-Studies', function (req, res, next) {
-  res.render('major', { major: "International Studies", gpa: gpaList[14] });
+  const IAClasses = Object.keys(jsonData).filter(key => jsonData[key] === "IA");
+  res.render('major', { major: "International Studies", gpa: gpaList[14],classes:IAClasses });
 });
 
 app.use('/Mathematics', function (req, res, next) {
-  res.render('major', { major: "Mathematics", gpa: gpaList[15] });
+  const MAClasses = Object.keys(jsonData).filter(key => jsonData[key] === "MA");
+  res.render('major', { major: "Mathematics", gpa: gpaList[15],classes:MAClasses});
 });
 
 app.use('/Mechanical-Engineering', function (req, res, next) {
-  res.render('major', { major: "Mechanical Engineering", gpa: gpaList[16] });
+  const MEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "ME");
+  res.render('major', { major: "Mechanical Engineering", gpa: gpaList[16],classes:MEClasses});
 });
 
 app.use('/Optical-Engineering', function (req, res, next) {
-  res.render('major', { major: "Optical Engineering", gpa: gpaList[17] });
+  const OEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "OE");
+  res.render('major', { major: "Optical Engineering", gpa: gpaList[17],classes:OEClasses });
 });
 
 app.use('/Physics', function (req, res, next) {
-  res.render('major', { major: "Physics", gpa: gpaList[18] });
+  const PHClasses = Object.keys(jsonData).filter(key => jsonData[key] === "PH");
+  res.render('major', { major: "Physics", gpa: gpaList[18],classes:PHEClasses });
 });
 
 app.use('/Software-Engineering', function (req, res, next) {
-  res.render('major', { major: "Software Engineering", gpa: gpaList[19] });
+  const CSSEClasses = Object.keys(jsonData).filter(key => jsonData[key] === "CSSE");
+  res.render('major', { major: "Software Engineering", gpa: gpaList[19],classes:CSSEClasses });
 });
 
 
