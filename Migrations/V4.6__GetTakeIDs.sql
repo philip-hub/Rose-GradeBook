ed7ec4f141c4e4b08204d81b27832f7c65692779
gajavegs <gauravsg2004@gmail.com>
2024-04-20 01:00:48 -0400

    Update V4.6__GetTakeIDs.sql

--- Migrations/V4.6__GetTakeIDs.sql
+++ Migrations/V4.6__GetTakeIDs.sql
@@ -1,1 +1,28 @@
+CREATE TYPE GradeDataAndSectionIDs AS TABLE ( 
+
+  Sd int, -- sectionid
+  Ge float -- grade
+)
+
+--https://www.mssqltips.com/sqlservertip/1483/using-table-valued-parameters-tvp-in-sql-server/
+GO
+-- use new tech to match on four fields, esp professor name
+CREATE OR ALTER PROCEDURE getTakeIDs
+(@data GradeDataAndSectionIDs READONLY,
+@numRows int)
+AS
+BEGIN
+
+
+SELECT (
+  SELECT TakeID FROM Takes
+  WHERE 
+    TakeID IN (SELECT TOP (@numRows) TakeID FROM Takes ORDER BY TakeID DESC)
+    AND Sd=CourseID
+    AND Ge=Grade
+    )
+as TakeID
+FROM @data 
+END
+GO
 
