6371e6a369a12daacfbdcfdb2f67d68f05681eed
gajavegs <gauravsg2004@gmail.com>
2024-07-21 19:42:24 -0400

    Add Next.js frontend, with DaisyUI
    More setup description too

---
+++ Client/rose-gradebook/app/page.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import logo from '@/images/logo.png';
+
+export default function Home() {
+  return (
+    <html lang="en">
+      <body>
+        <div class="container">
+          <h1>Discovering Rose's real academic data</h1>
+          <Image src={logo} alt="Logo"/>
+          <br></br>
+          <form method="post" action="/show_VerifyEmailForm">
+          <input type="submit" name="submit" value="Signup"/>
+          </form>
+
+          
+        </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      </body>
+    </html>
+  );
+}
--- Setup.md
+++ Setup.md
@@ -2,7 +2,7 @@
 # STEP 1 - Node.js version
 Run brew list, uninstall any node and or any specific node versions with brew uninstall --force node
 
-Delete any node-related stuff from path if it's still there
+Delete any node-related stuff from path if it's still there (check it's deleted by rerunning brew list)
 
 Run brew install nvm
 
@@ -12,4 +12,10 @@ Add this line to it: source ~/.nvm/nvm.sh
 
 Run nvm use 22 (run nvm install 22 if v22 is not yet installed)
 
-Try running npm run start in /Server/package.json
+# STEP 2 - Running backend (Server)
+
+Try running npm run start in Rose-GradeBook/Server/ (npm install dependencies as needed)
+
+# STEP 3 - Running frontend (Client)
+
+Try running npm run dev in Rose-GradeBook/Client/rose-gradebook/ (npm install dependencies as needed)
